3631cc2644a35075e53ad0bbcdbe2f963018194d
sadashi ota <sadashi.ota@hispot.co.jp>
2018-04-20 05:54:16 +0900

    implements MainActivity

---
+++ app/src/main/java/com/sadashi/apps/firebase/auth/activities/MainActivity.java
@@ -0,0 +1,43 @@
+package com.sadashi.apps.firebase.auth.activities;
+
+import android.content.Intent;
+import android.support.annotation.StringDef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.sadashi.apps.firebase.auth.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PASSWORD_LESS_SAMPLE = "Password Less Sample";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        ListView listView = findViewById(R.id.sample_list);
+
+        String[] samples = {PASSWORD_LESS_SAMPLE};
+        listView.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, samples));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = (String) adapterView.getItemAtPosition(i);
+                switch (text) {
+                    case PASSWORD_LESS_SAMPLE:
+                        break;
+                }
+            }
+        });
+    }
+}
