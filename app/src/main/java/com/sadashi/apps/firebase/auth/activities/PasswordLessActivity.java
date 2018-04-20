a644cabdde27eb1c21e245970ac009cc800035fc
sadashi ota <sadashi.ota@hispot.co.jp>
2018-04-20 09:14:37 +0900

    Add PasswordLessActivity

---
+++ app/src/main/java/com/sadashi/apps/firebase/auth/activities/PasswordLessActivity.java
@@ -0,0 +1,15 @@
+package com.sadashi.apps.firebase.auth.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sadashi.apps.firebase.auth.R;
+
+public class PasswordLessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_less);
+    }
+}
